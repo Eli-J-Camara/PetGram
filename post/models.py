92b03af248fdd888892659eb371f5ba0d4bedd0c
Latisha Johnson <latishaj966@gmail.com>
2021-04-09 14:52:56 -0700

    got the validiators to work so you can not upload any file

--- post/models.py
+++ post/models.py
@@ -2,10 +2,11 @@ from django.db import models
 from django.utils import timezone
 from user_profile.models import CustomUser
 from tagulous.models import TagField
+from django.core.validators import FileExtensionValidator
 
 class Post(models.Model):
     display_name = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
-    post_file= models.FileField(upload_to='post/', null=True)
+    post_file= models.FileField(upload_to='post/', validators=[FileExtensionValidator(['jpg','jpeg','mp4', 'mov'])], null=True)
     # post_vid= models.FileField(upload_to='video/', null=True, blank=True)
     caption = models.TextField()
     created_at = models.DateTimeField(default=timezone.now)
